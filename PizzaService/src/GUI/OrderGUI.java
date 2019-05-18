2e540189053cbbfb74a5d09190bd6a1b8b57574c
LukeHubWalker <48350602+LukeHubWalker@users.noreply.github.com>
2019-05-18 10:49:49 +0200

    Moin

--- PizzaService/src/GUI/OrderGUI.java
+++ PizzaService/src/GUI/OrderGUI.java
@@ -335,7 +335,7 @@ public class OrderGUI extends Application {
 			}
 		});
 
-
+		///
 		Button btnGutschein = new Button("Gutschein 10%");
 		btnGutschein.setOnAction(new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent e) {
